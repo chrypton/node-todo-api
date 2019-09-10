843a10173bd42c11abbc6a1778d43788ca4583d5
Asim Ibn Asghar <asim.ibn.asghar@gmail.com>
2019-09-10 22:44:20 +0100

    env variables added

--- sandbox/mongoose-remove.js
+++ sandbox/mongoose-remove.js
@@ -8,13 +8,13 @@ const {User} = require('./../server/model/user');
 //   console.log(result);
 // });
 
-// Todo.findOneAndRemove
-// Todo.findByIdAndRemove
+// Todo.findOneAndDelete
+// Todo.findByIdAndDelete
 
-// Todo.findOneAndRemove({_id: '57c4610dbb35fcbf6fda1154'}).then((todo) => {
+// Todo.findOneAndDelete({_id: '57c4610dbb35fcbf6fda1154'}).then((todo) => {
 //
 // });
 
-Todo.findByIdAndRemove('57c4610dbb35fcbf6fda1154').then((todo) => {
+Todo.findByIdAndDelete('57c4610dbb35fcbf6fda1154').then((todo) => {
   console.log(todo);
 });
