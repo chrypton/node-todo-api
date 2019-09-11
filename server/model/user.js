58d56468da7064ba06effa8244160b22115536c5
Asim Ibn Asghar <asim.ibn.asghar@gmail.com>
2019-09-11 07:27:29 +0100

    Add Post /users route and token method

--- server/model/user.js
+++ server/model/user.js
@@ -1,12 +1,72 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-var User = mongoose.model('User', {
+var UserSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
         trim: true,
-        minlength: 1
-    }
+        minlength: 1,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: `{VALUE} is not a valid email`
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
 
+UserSchema.methods.toJSON = function () {
+    var user = this;
+    var userObject = user.toObject();
+
+    return _.pick(userObject, ['_id', 'email']);
+};
+
+UserSchema.methods.generateAuthToken = function () {
+    var user = this;
+    var access = 'auth';
+    var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+    // MongoDB's versions have inconsistency and Array.push() method doesnt work as expected.
+    user.tokens = user.tokens.concat([{access, token}]);
+
+    // return user so we can chain .then() promise on server.js
+    return user.save().then(() => {
+                return token;
+    });
+};
+
+var User = mongoose.model('User', UserSchema);
+
 module.exports = { User };
+
+
+
+
+
+
+// {
+//     email: 'tuityfruity@example.com',
+//     password: 'hsggwyiojzb2ut81ika7oap0o1',
+//     tokens: [{
+//       access: 'auth',
+//       token: 'iojzb2uhsggwyiojzb2ut81ika7oap0o1'
+//     }]
+// }
